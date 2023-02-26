8d79681a7e8ebded9951bcb1d86c357d2957664e
unknown <jacemcarr@gmail.com>
2023-02-26 13:38:33 -0700

    Fixed: project-nav spacing from right edge is statically assigned to be equivalent to 40px, but it does not change when resizing windows like other clamp positions do, changed some fonts, made the project-title and project-summary act as a single link button, added a hover effect to the parent div of the prior two. New: added a contact box with input fields (doesn't work and input text needs to be changed to black).

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
-function Contact () {
-    return (
-      <div className="contact-content">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur,
-        quasi. Eum nesciunt minima, ullam blanditiis doloribus quidem architecto
-        neque dolor.
-      </div>
-    );
-  }
-
+function Contact() {
+  return (
+    <div className="contact-content">
+      <h2>Contact Me</h2>
+      <form
+        action="mailto:jacemcarr@gmail.com"
+        method="post"
+        enctype="text/plain"
+      >
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required></input>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required></input>
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>
+        <input type="submit" value="Send"></input>
+      </form>
+    </div>
+  );
+}
 
 export default Contact;
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,16 +2,25 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="author-summary-aligner">
-      <section className="author-summary">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto
-        repellat soluta voluptate atque animi ratione reiciendis quidem! Porro
-        repellat nobis eligendi nihil! Iste atque tempore ad unde temporibus
-        doloribus impedit facilis quaerat quidem inventore odit enim debitis sit
-        dignissimos deserunt libero corporis necessitatibus vel cumque, ex
-        distinctio odio praesentium.
-      </section>
-    </div>
+    <section className="author-summary">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto
+      repellat soluta voluptate atque animi ratione reiciendis quidem! Porro
+      repellat nobis eligendi nihil! Iste atque tempore ad unde temporibus
+      doloribus impedit facilis quaerat quidem inventore odit enim debitis sit
+      dignissimos deserunt libero corporis necessitatibus vel cumque, ex
+      distinctio odio praesentium. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Tempore, aliquam at suscipit deleniti quidem nobis
+      debitis quaerat unde eaque voluptatum maiores impedit nisi doloremque
+      laudantium ad, officiis facilis nihil repellat expedita, ab eos earum
+      mollitia! Error doloribus dignissimos quisquam deserunt delectus sit
+      voluptates. Reprehenderit itaque sit magni totam consequatur, sequi enim
+      ratione assumenda repudiandae. Esse vero libero vitae labore, quam
+      quibusdam necessitatibus voluptate consectetur, dignissimos provident illo
+      saepe velit doloribus reprehenderit tempora laboriosam, maiores quasi
+      repellat quas ipsa corrupti odit cumque rerum nihil? Facilis libero
+      aliquid quis dicta illo voluptates commodi corrupti nesciunt ea veniam,
+      ipsam nobis?
+    </section>
   );
 }
 
