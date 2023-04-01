5c5c245f6c5082fa7cbcf210b2d496f093963ea1
unknown <jacemcarr@gmail.com>
2023-04-01 04:12:03 -0600

    fixed image dynamic scaling issues, decommisioned covid tracker live embed in favor of scaling images of dash with link to interactive dash

--- src/components/projects/project3.jsx
+++ src/components/projects/project3.jsx
@@ -1,8 +1,8 @@
 import { opacityFilter, addHorizontalNavbar } from "../../utils/functions";
-import React, { useRef, useEffect, useCallback } from "react";
+import React from "react";
 import "./project3.css";
-
-const { tableau } = window;
+import trackerHome from "../../assets/images/project3/tracker-home.png";
+import trackerBar from "../../assets/images/project3/tracker-bar.png";
 
 function Project3() {
   setTimeout(() => {
@@ -10,41 +10,34 @@ function Project3() {
   }, 10);
   addHorizontalNavbar();
 
-  const ref = useRef(null);
-  const url =
-    "https://public.tableau.com/views/coviddashboard_16748574137170/CovidTrackerDashboard1";
-
-  const vizRef = useRef(null);
-
-  const initViz = useCallback(() => {
-    if (vizRef.current) {
-      vizRef.current.dispose();
-    }
-    const options = {
-      width: "100%",
-      height: "100%",
-      hideTabs: true,
-      suppressAlerts: true, // add this option to suppress warnings
-    };
-    vizRef.current = new tableau.Viz(ref.current, url, options);
-  }, [url]);
-
-  useEffect(() => {
-    initViz();
-
-    return () => {
-      if (vizRef.current) {
-        vizRef.current.dispose();
-      }
-    };
-  }, [initViz, vizRef]);
-
   return (
-    <div className="body">
-      <div className="project3-container">
-        <div ref={ref} className="tableau-viz" />
+    <section className="project3-container">
+      <div className="grid1">
+        <div className="section-title">Introduction</div>
+        <p>
+          A fully interactive tableau dashboard utilizing active-filtering of
+          data parameters (such as date range, country, continent) based on user
+          input. The data set used is a study of the impact of the first year of
+          COVID-19, with the dashboard specifically highlighting vaccinnation
+          rates on a global scale.
+        </p>
+        <br />
+        <div className="section-title">Dashboard Images</div>
+        <p>
+          Images of the interactive tableau dashboard. The actual dashboard can
+          be viewed by clicking the code link in the project description on the
+          projects page.
+        </p>
+        <br />
+        <div className="image">
+          <img src={trackerHome} alt="covid dashboard home" />
+        </div>
+        <br />
+        <div className="image">
+          <img src={trackerBar} alt="covid dashboard barchart" />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
